a4a43464ba3d3328916f12072484cdb7f343be7b
mikaeltje <112475540+mikaeltje@users.noreply.github.com>
2023-06-06 13:50:51 +0200

    upload again

---
+++ src/js/scrollingBackground.js
@@ -0,0 +1,37 @@
+import { Actor, Vector, GraphicsGroup } from 'excalibur';
+import { Resources } from './resources.js'
+
+export class Background extends Actor {
+    offset;
+
+    onInitialize(engine) {
+        const BackgroundImage = Resources.Background.toSprite();
+        this.offset = BackgroundImage.width;
+
+        BackgroundImage.height = 600;
+
+        const group = new GraphicsGroup({
+            members: [
+                {
+                    graphic: BackgroundImage,
+                    pos: new Vector(0, 0),
+                },
+                {
+                    graphic: BackgroundImage,
+                    pos: new Vector(BackgroundImage.width, 0),
+                }
+            ]
+        });
+
+        this.graphics.anchor = new Vector(0, 0);
+        this.graphics.add(group);
+        this.pos = new Vector(0, 0);
+        this.vel = new Vector(-100, 0);
+    }
+
+    onPostUpdate(engine, delta) {
+        if (this.pos.x < -this.offset) {
+            this.pos = new Vector(0, 0);
+        }
+    }
+}
